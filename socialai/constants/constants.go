19e093e11248403afec83ae52075b4eed0f5d1d8
Jiawei Meng <103376411+missionary0326@users.noreply.github.com>
2025-10-28 20:21:18 -0400

    Social-AI backend

---
+++ socialai/constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL     = "https://10.128.0.2:9200"
+    ES_USERNAME = "jarvis"
+    ES_PASSWORD = "welcome"
+	GCS_BUCKET  = "socialai_25" 
+)
